c4b8177dcae7ccd0e11aae6e1e22ddd7efdd11cf
Juan Cobian <cobianjuan19@gmail.com>
2020-08-20 23:40:41 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(json => this.setState({data: json}))
+  }
+
   render() {
 
   }
